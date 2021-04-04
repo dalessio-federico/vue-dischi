d503afd0dc0345990563d37218a9af036ddeac5f
federico d'alessio <federico1993.d.alessio.1993@gmail.com>
2021-04-04 14:13:12 +0200

    esercizio completo

---
+++ js/master.js
@@ -0,0 +1,23 @@
+Vue.config.devtools = true;
+
+var root = new Vue({
+    
+    el: "#root",
+
+    data : {
+        albumList : []
+    },
+
+    mounted(){
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((r) => 
+            {
+                r.data.response.forEach(e => {
+                    this.albumList.push(e)
+                });
+                console.log(this.albumList)
+            });
+    } 
+
+})
