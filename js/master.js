7db020c44fa6a52503c259926a7a885844802f78
federico d'alessio <federico1993.d.alessio.1993@gmail.com>
2021-04-06 10:07:04 +0200

    aggiunta visulizazione dinamica

--- js/master.js
+++ js/master.js
@@ -6,6 +6,7 @@ var root = new Vue({
 
     data : {
         albumList : [],
+        albumGender : [],
         gender : ""
     },
 
@@ -15,10 +16,16 @@ var root = new Vue({
             .then((r) => 
             {
                 r.data.response.forEach(e => {
-                    this.albumList.push(e)
+                    this.albumList.push(e);
+                    if(this.albumGender.includes(e.genre))
+                    {
+                        null;
+                    }
+                    else
+                    {
+                        this.albumGender.push(e.genre);
+                    }
                 });
-                console.log(this.albumList)
             });
-    } 
-
+    }
 })
